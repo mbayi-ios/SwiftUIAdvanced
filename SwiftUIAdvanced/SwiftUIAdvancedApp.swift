c509a060f65cd1f0bd5f870e36a55ed5b5526766
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-18 22:15:54 +0300

    Initial Commit

---
+++ SwiftUIAdvanced/SwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedApp.swift
+//  SwiftUIAdvanced
+//
+//  Created by Amby on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
