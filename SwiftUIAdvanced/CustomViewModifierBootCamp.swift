e8f6e5b3b50e4a087061813173253703cc7e8da5
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-08 23:47:54 +0300

    custom modifiers

--- SwiftUIAdvanced/CustomViewModifierBootCamp.swift
+++ SwiftUIAdvanced/CustomViewModifierBootCamp.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct DefaultButtonViewModifier: ViewModifier {
-
+    let backgroundColor: Color
     func body(content: Content) -> some View {
         content
             .font(.headline)
             .foregroundColor(.white)
             .frame(height: 55)
             .frame(maxWidth: .infinity)
-            .background(Color.blue)
+            .background(backgroundColor)
             .cornerRadius(10)
             .shadow(radius: 10)
             .padding()
 
-        //need to check on the modiifers to put inside the custom
+        //need to check on the modiifers to put inside the custom (fonts, padding) shouldnt be put in custom modifier
     }
     
 }
 extension View {
-    func withDefaultButtonFormatting() -> some View {
-        self.modifier(DefaultButtonViewModifier())
+    func withDefaultButtonFormatting(backgroundColor: Color) -> some View {
+        self.modifier(DefaultButtonViewModifier(backgroundColor: backgroundColor))
     }
 }
 
@@ -35,10 +35,10 @@ struct CustomViewModifierBootCamp: View {
         VStack {
 
             Text("Hello World Me")
-                .modifier(DefaultButtonViewModifier()) // works as default modifier
+                .modifier(DefaultButtonViewModifier(backgroundColor: .pink)) // works as default modifier
 
             Text("Hello World")
-                .withDefaultButtonFormatting() // using view extension to work like .font()
+                .withDefaultButtonFormatting(backgroundColor: .yellow) // using view extension to work like .font()
 
 
             Text("Hello World")
