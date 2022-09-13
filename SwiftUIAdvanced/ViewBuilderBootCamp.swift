380dd5eecae1cddeb60e52a4ffe6d8bf6d2a124c
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-13 21:54:00 +0300

    using switch statement

--- SwiftUIAdvanced/ViewBuilderBootCamp.swift
+++ SwiftUIAdvanced/ViewBuilderBootCamp.swift
@@ -12,48 +12,48 @@ struct HeaderViewRegular: View {
     let title: String
     let description: String?
     let iconName: String?
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(title)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
-
+            
             if let description = description {
                 Text("description")
                     .font(.callout)
             }
-
+            
             if let iconName = iconName {
                 Image(systemName: iconName)
             }
-
+            
             RoundedRectangle(cornerRadius: 5)
                 .frame(height: 2)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
     }
-
+    
 }
 
 struct HeaderViewGeneric<Content:View>: View {
     let title: String
     let content: Content
-
+    
     init(title: String, @ViewBuilder content: () -> Content) {
         self.title = title
         self.content = content()
     }
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(title)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
-
+            
             content
-
+            
             RoundedRectangle(cornerRadius: 5)
                 .frame(height: 2)
         }
@@ -64,11 +64,11 @@ struct HeaderViewGeneric<Content:View>: View {
 
 struct CustomHStack<Content: View>: View {
     let content: Content
-
+    
     init(@ViewBuilder content: () -> Content) {
         self.content = content()
     }
-
+    
     var body: some View {
         HStack {
             content
@@ -81,32 +81,32 @@ struct ViewBuilderBootCamp: View {
         VStack {
             HeaderViewRegular(title: "Title One", description: "hello", iconName: "heart.fill")
             HeaderViewRegular(title: "Title One", description: nil, iconName: nil)
-
+            
             Divider()
             //
             //            HeaderViewGeneric(title: "Generic Title", content: Text("hello"))
             //
             //            HeaderViewGeneric(title: "generic 2", content: Image(systemName: "heart.fill"))
-
+            
             HeaderViewGeneric(title: "Generic Title") {
-
+                
                 HStack {
                     Text("Hi View Builder")
                     Image(systemName: "heart.fill")
                 }
-
+                
             }
-
+            
             CustomHStack {
                 Text("hello")
                 Text("hello")
                 Text("hello")
             }
-
-
+            
+            
             Spacer()
         }
-
+        
     }
 }
 
@@ -118,36 +118,37 @@ struct ViewBuilderBootCamp_Previews: PreviewProvider {
 
 
 struct LocalViewBuilder: View {
-
+    
     enum ViewType {
         case one, two, three
     }
-
+    
     let type: ViewType
-
+    
     var body: some View {
         VStack {
             headerSection
         }
     }
-
+    
     @ViewBuilder private var headerSection: some View {
-        if type == .one {
+        
+        switch   type {
+        case .one:
             viewOne
-
-        } else if type == .two {
+        case .two:
             viewTwo
-
-        } else if type == .three {
+        case .three:
             viewThree
         }
+        
     }
-
+    
     private var viewOne: some View {
         Text("one")
     }
     private var viewTwo: some View {
-
+        
         VStack {
             Text("two and")
             Image(systemName: "heart.fill")
@@ -155,6 +156,6 @@ struct LocalViewBuilder: View {
     }
     private var viewThree: some View {
         Image(systemName: "heart.fill")
-
+        
     }
 }
